e1014629b53f7b5d52b4d7ffcca82cf34d12d98c
niloCER <102003372+niloCER@users.noreply.github.com>
2022-03-21 08:06:56 -0400

    server name added

--- app.py
+++ app.py
@@ -164,6 +164,11 @@ app = dash.Dash(
     external_stylesheets=[dbc.themes.SUPERHERO]
 )
 
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
 app.layout = html.Div([
 
 
